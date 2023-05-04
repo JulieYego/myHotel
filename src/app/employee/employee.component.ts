381f031e75478c9c33191d1a9edacd3791bdfe1f
Julie Yego <julieyego@strathmore.edu>
2023-05-05 00:28:40 +0300

    services - logger and rooms

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Self } from '@angular/core';
+import { RoomsService } from '../rooms/services/rooms.service';
 
 @Component({
   selector: 'hinv-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
+  providers: [RoomsService] // local instance
 })
 export class EmployeeComponent implements OnInit{
   empName : string = 'Dylan'
 
-  constructor() {}
+  constructor(@Self() private roomsService : RoomsService) {}
 
   ngOnInit(): void {
   }
-
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { HeaderComponent } from './../header/header.component';
-import { AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit, QueryList, SkipSelf, ViewChild, ViewChildren } from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { RoomsService } from './services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms',
@@ -34,38 +35,11 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit, AfterView
   //   this.childComponent.sayHello()
   // }
 
-  constructor() {}
+  constructor(@SkipSelf() private roomsService : RoomsService) {}
   
   ngOnInit() : void {
-    console.log(this.headerComponent)
-    this.roomList = [{
-      roomNumber: 1,
-      roomType : "Deluxe Room",
-      amenities : "Air Conditioning, Free Wi-Fi, TV, Bathroom, City View",
-      price : 1500,
-      photos : "https://unsplash.com/photos/VGs8z60yT2c",
-      checkinTime : new Date('12-Feb-2023'),
-      checkoutTime : new Date('16-Feb-2023'),
-      rating: 4.5
-    },{
-      roomNumber: 2,
-      roomType : "Standard Room",
-      amenities : "Air Conditioning, Free Wi-Fi, Bathroom",
-      price : 500,
-      photos : "https://unsplash.com/photos/oxeCZrodz78",
-      checkinTime : new Date('28-Feb-2023'),
-      checkoutTime : new Date('1-Mar-2023'),
-      rating: 3.141592
-    },{
-      roomNumber: 3,
-      roomType : "Private Suite",
-      amenities : "Air Conditioning, Free Wi-Fi, TV, Bathroom, Kitchenette, Living Room",
-      price : 15000,
-      photos : "https://unsplash.com/photos/KFDuhyW5H5w",
-      checkinTime : new Date('7-Apr-2023'),
-      checkoutTime : new Date('12-Apr-2023'),
-      rating: 4.8
-    }]
+    //console.log(this.headerComponent)
+    this.roomList = this.roomsService.getRooms()
   }
 
   ngDoCheck(): void {
